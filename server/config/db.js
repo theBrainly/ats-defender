c4ada49df5a9c92cedb99fd4ea6b82b0798f838e
Akash Sharma <officialmailakashsharma@gmail.com>
2026-01-05 13:54:00 +0000

    feat: activate connectDB module logic

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 import dotenv from "dotenv"
 dotenv.config({ path: './.env' });
+/**
+ * Active: 2026-01-05
+ * Function: connectDB
+ */
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URL);
