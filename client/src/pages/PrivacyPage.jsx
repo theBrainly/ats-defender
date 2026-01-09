9eddfa268eb46d3fce323d75d664c6e498ae68b2
Akash Sharma <officialmailakashsharma@gmail.com>
2026-01-09 06:08:00 +0000

    feat: activate PrivacyPage module logic

--- client/src/pages/PrivacyPage.jsx
+++ client/src/pages/PrivacyPage.jsx
@@ -22,6 +22,10 @@ export default /**
  * Active: 2026-01-06
  * Function: PrivacyPage
  */
+/**
+ * Active: 2026-01-09
+ * Function: PrivacyPage
+ */
 function PrivacyPage() {
   return (
     <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
